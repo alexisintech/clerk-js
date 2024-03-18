95b88cc2444d30152940f186bb4aa964843c6a87
Alexis Aguilar <98043211+alexisintech@users.noreply.github.com>
2024-03-18 13:03:53 -0400

    working base

--- src/main.js
+++ src/main.js
@@ -12,34 +12,6 @@ await clerk.load();
 
 if (clerk.user) {
   console.log(clerk.user);
-  // document.getElementById("create-totp").addEventListener("click", async () => {
-  //   clerk.user
-  //     .createTOTP()
-  //     .then((res) => {
-  //       console.log(res);
-  //       document.getElementById("enter-totp-secret").removeAttribute("hidden");
-  //       document.getElementById("totp-secret").innerHTML = res.secret;
-  //       document.getElementById("verify-totp-form").removeAttribute("hidden");
-  //       document
-  //         .getElementById("verify-totp")
-  //         .addEventListener("click", async () => {
-  //           clerk.user
-  //             .verifyTOTP(document.getElementById("totp-code").value)
-  //             .then((res) => {
-  //               console.log(res);
-  //             })
-  //             .catch((error) => {
-  //               document.getElementById("error").innerHTML =
-  //                 error.errors[0].message;
-  //               console.log("An error occurred:", error.errors);
-  //             });
-  //         });
-  //     })
-  //     .catch((error) => {
-  //       document.getElementById("error").innerHTML = error.errors[0].message;
-  //       console.log("An error occurred:", error.errors);
-  //     });
-  // });
 
   // Mount user profile
   const userProfileComponent = document.getElementById("user-profile");
@@ -67,6 +39,126 @@ if (clerk.user) {
   // Mount create org button
   const createOrgDiv = document.getElementById("create-org");
   clerk.mountCreateOrganization(createOrgDiv);
+
+  // MEMBERSHIPS LIST
+  const data = await clerk.user.getOrganizationMemberships();
+  console.log(`Organization Memberships:`, data);
+
+  async function renderMemberships(organization, isAdmin) {
+    const list = document.getElementById("memberships_list");
+    try {
+      const memberships = await organization.getMemberships();
+      console.log(`getMemberships:`, memberships);
+
+      memberships.map((membership) => {
+        const li = document.createElement("li");
+        li.textContent = `ID: ${membership.id} Identifier: ${membership.publicUserData.identifier} UserId: ${membership.publicUserData.userId} Role: ${membership.role} Permissions: ${membership.permissions}`;
+
+        // Add administrative actions; update role and remove member.
+        if (isAdmin) {
+          const updateBtn = document.createElement("button");
+          updateBtn.textContent = "Change role";
+          updateBtn.addEventListener("click", async function (e) {
+            e.preventDefault();
+            const role = membership.role === "admin" ? "org:member" : "admin";
+            await membership.update({ role });
+          });
+          li.appendChild(updateBtn);
+
+          const removeBtn = document.createElement("button");
+          removeBtn.textContent = "Remove";
+          removeBtn.addEventListener("click", async function (e) {
+            e.preventDefault();
+            await currentOrganization.removeMember(membership.userId);
+          });
+          li.appendChild(removeBtn);
+        }
+
+        // Add the entry to the list
+        list.appendChild(li);
+      });
+    } catch (err) {
+      console.error(err);
+    }
+  }
+
+  // INVITATIONS LIST
+  async function renderInvitations(organization, isAdmin) {
+    const list = document.getElementById("invitations_list");
+    try {
+      const { totalCount, data } = await organization.getInvitations();
+
+      const invitations = data;
+
+      if (invitations.length === 0) {
+        list.textContent = "No invitations";
+      }
+
+      invitations.map((invitation) => {
+        const li = document.createElement("li");
+        li.textContent = `${invitation.emailAddress} - ${invitation.role}`;
+
+        // Add administrative actions; revoke invitation
+        if (isAdmin) {
+          const revokeBtn = document.createElement("button");
+          revokeBtn.textContent = "Revoke";
+          revokeBtn.addEventListener("click", async function (e) {
+            e.preventDefault();
+            await invitation.revoke();
+          });
+          li.appendChild(revokeBtn);
+        }
+        // Add the entry to the list
+        list.appendChild(li);
+      });
+    } catch (err) {
+      console.error(err);
+    }
+  }
+
+  async function init() {
+    // This is the current organization ID.
+    const organizationId = clerk.organization.id;
+
+    const organizationMemberships =
+      await clerk.user.getOrganizationMemberships();
+
+    const currentMembership = organizationMemberships.find(
+      (membership) => membership.organization.id === organizationId
+    );
+    const currentOrganization = currentMembership.organization;
+
+    if (!currentOrganization) {
+      return;
+    }
+    const isAdmin = currentMembership.role === "org:admin";
+
+    renderMemberships(currentOrganization, isAdmin);
+    renderInvitations(currentOrganization, isAdmin);
+
+    if (isAdmin) {
+      const form = document.getElementById("new_invitation");
+      form.addEventListener("submit", async function (e) {
+        e.preventDefault();
+        const inputEl = document.getElementById("email_address");
+        if (!inputEl) {
+          return;
+        }
+
+        try {
+          console.log(inputEl.value);
+          await currentOrganization.inviteMember({
+            emailAddress: inputEl.value,
+            role: "org:member",
+          });
+        } catch (err) {
+          console.error(err);
+        }
+      });
+    }
+  }
+
+  init();
 } else {
   // Mount sign in component
   document.getElementById("app").innerHTML = `
