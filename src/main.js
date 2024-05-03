63752bbc34822af1bf9176e66fba1f44152a2464
Alexis Aguilar <98043211+alexisintech@users.noreply.github.com>
2024-05-03 13:12:45 -0400

    update flow

--- src/main.js
+++ src/main.js
@@ -1,9 +1,9 @@
-import { Clerk } from "@clerk/clerk-js";
+import { Clerk } from '@clerk/clerk-js';
 
 const pubKey = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
 
 if (!pubKey) {
-  throw new Error("Add your VITE_CLERK_PUBLISHABLE_KEY to .env file");
+  throw new Error('Add your VITE_CLERK_PUBLISHABLE_KEY to .env file');
 }
 
 const clerk = new Clerk(pubKey);
@@ -11,52 +11,75 @@ await clerk.load();
 
 if (clerk.user) {
   // Mount user button component
-  document.getElementById("signed-in").innerHTML = `
+  document.getElementById('signed-in').innerHTML = `
     <div id="user-button"></div>
   `;
 
-  const userbuttonDiv = document.getElementById("user-button");
+  const userbuttonDiv = document.getElementById('user-button');
 
   clerk.mountUserButton(userbuttonDiv);
 } else {
-  // Handle the sign-up form
+  const errorContainer = document.getElementById('error');
+
+  // Handle the sign-in form
   document
-    .getElementById("sign-up-form")
-    .addEventListener("submit", async (e) => {
+    .getElementById('sign-in-form')
+    .addEventListener('submit', async (e) => {
       e.preventDefault();
 
       const formData = new FormData(e.target);
-      const phone = formData.get("phone");
+      const phone = formData.get('phone');
 
       try {
-        // Start the sign-up process using the phone number method
-        await clerk.client.signUp.create({
-          phoneNumber: phone,
+        // Start the sign-up process using the user's identifier. In this case, it's their phone number.
+        const { supportedFirstFactors } = await clerk.client.signIn.create({
+          identifier: phone,
         });
-        await clerk.client.signUp.preparePhoneNumberVerification();
-        // Hide sign-up form
-        document.getElementById("sign-up").setAttribute("hidden", "");
+
+        // Find the phoneNumberId from all the available first factors for the current sign-in
+        const firstPhoneFactor = supportedFirstFactors.find((factor) => {
+          return factor.strategy === 'phone_code';
+        });
+
+        const { phoneNumberId } = firstPhoneFactor;
+
+        // Prepare first factor verification, specifying
+        // the phone code strategy.
+        await clerk.client.signIn.prepareFirstFactor({
+          strategy: 'phone_code',
+          phoneNumberId,
+        });
+
+        // Hide sign-in form
+        document.getElementById('sign-in').setAttribute('hidden', '');
         // Show verification form
-        document.getElementById("verifying").removeAttribute("hidden");
+        document.getElementById('verifying').removeAttribute('hidden');
+        // Clear any previous errors
+        errorContainer.setAttribute('hidden', '');
       } catch (error) {
+        errorContainer.removeAttribute('hidden');
+        errorContainer.innerHTML = error.errors[0].longMessage;
         console.error(error);
       }
     });
 
   // Handle the verification form
-  document.getElementById("verifying").addEventListener("submit", async (e) => {
+  document.getElementById('verifying').addEventListener('submit', async (e) => {
     const formData = new FormData(e.target);
-    const code = formData.get("code");
+    const code = formData.get('code');
 
     try {
-      // Verify the email address
-      const verify = await clerk.client.signUp.attemptPhoneNumberVerification({
+      // Verify the phone number
+      const verify = await clerk.client.signIn.attemptFirstFactor({
+        strategy: 'phone_code',
         code,
       });
 
       // Now that the user is created, set the session to active.
       await clerk.setActive({ session: verify.createdSessionId });
     } catch (error) {
+      errorContainer.removeAttribute('hidden');
+      errorContainer.innerHTML = error.errors[0].longMessage;
       console.error(error);
     }
   });
