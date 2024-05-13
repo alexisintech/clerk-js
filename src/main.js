8821ad24dd8d9b922ec8b51a6fc837b3f0f480ed
Alexis Aguilar <98043211+alexisintech@users.noreply.github.com>
2024-05-13 12:35:13 -0400

    add example

--- src/main.js
+++ src/main.js
@@ -20,68 +20,8 @@ if (clerk.user) {
   const userbuttonDiv = document.getElementById('user-button');
 
   clerk.mountUserButton(userbuttonDiv);
-} else {
-  // Handle the sign-in form
-  document
-    .getElementById('sign-in-form')
-    .addEventListener('submit', async (e) => {
-      e.preventDefault();
 
-      const formData = new FormData(e.target);
-      const emailAddress = formData.get('email');
-      const password = formData.get('password');
-
-      try {
-        // Start the sign-in process
-        await clerk.client.signIn.create({
-          identifier: emailAddress,
-          password,
-        });
-
-        // Hide sign-in form
-        document.getElementById('sign-in').setAttribute('hidden', '');
-        // Show verification form
-        document.getElementById('verifying').removeAttribute('hidden');
-      } catch (error) {
-        // See https://clerk.com/docs/custom-flows/error-handling
-        // for more info on error handling
-        console.error(error);
-      }
-    });
-
-  // Handle the verification form
-  document.getElementById('verifying').addEventListener('submit', async (e) => {
-    const formData = new FormData(e.target);
-    const totp = formData.get('totp');
-    const backupCode = formData.get('backupCode');
-
-    console.log(backupCode);
-
-    try {
-      const useBackupCode = backupCode ? true : false;
-      const code = backupCode ? backupCode : totp;
-
-      // Attempt the TOTP or backup code verification
-      const signInAttempt = await clerk.client.signIn.attemptSecondFactor({
-        strategy: useBackupCode ? 'backup_code' : 'totp',
-        code: code,
-      });
-
-      // If verification was completed, set the session to active
-      // and redirect the user
-      if (signInAttempt.status === 'complete') {
-        await clerk.setActive({ session: signInAttempt.createdSessionId });
-
-        location.reload();
-      } else {
-        // If the status is not complete, check why. User may need to
-        // complete further steps.
-        console.error(signInAttempt);
-      }
-    } catch (error) {
-      // See https://clerk.com/docs/custom-flows/error-handling
-      // for more info on error handling
-      console.error(error);
-    }
+  document.getElementById('sign-out').addEventListener('click', async () => {
+    await clerk.signOut();
   });
 }
